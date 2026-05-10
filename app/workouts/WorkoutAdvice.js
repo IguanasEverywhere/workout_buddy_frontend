38a9c7644548421cc0e513dcbbdb9c911688bcd0
Scott Schwab <scottschwab86@gmail.com>
2026-05-10 07:57:15 -0500

    Add state to button click to refresh workout advice fetch

--- app/workouts/WorkoutAdvice.js
+++ app/workouts/WorkoutAdvice.js
@@ -7,6 +7,13 @@ function WorkoutAdvice() {
   const [workoutAdvice, setWorkoutAdvice] = useState('');
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [getAdvice, setGetAdvice] = useState(false);
+
+
+
+  const handleClick = () => {
+    setGetAdvice(prevVal => !prevVal)
+  }
 
   useEffect(() => {
     fetch('http://127.0.0.1:5555/next-workout/1')
@@ -19,14 +26,14 @@ function WorkoutAdvice() {
       setError(err);
       setLoading(false);
     });
-  }, []);
+  }, [getAdvice]);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error loading workouts.</p>;
 
   return (
     <>
-      <button>Click to get workout advice</button>
+      <button onClick={handleClick}>Click to get workout advice</button>
       <p>{workoutAdvice.workout_advice}</p>
     </>
   )
